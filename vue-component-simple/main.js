7e71ca0b319e7c7bc65005ff26e69039e89c1be5
Sergio Soares <acad.sergio@gmail.com>
2017-12-08 11:54:05 -0300

    Improving Vue Component

--- vue-component-simple/main.js
+++ vue-component-simple/main.js
@@ -1,8 +1,23 @@
+Vue.component('task-list', {
+    template: '<div> <task v-for="task in tasks"> {{ task.title }}</task> </div>',
+
+    data() {
+        return {
+            tasks: [
+                { title: 'Study Vue', completed: false },
+                { title: 'Study Vue', completed: false },
+                { title: 'Study JS', completed: false },
+                { title: 'Study AWS', completed: false },
+                { title: 'Study TDD', completed: false }
+            ]
+        }
+    }
+})
+
 Vue.component('task', {
     template: '<li> <slot> </slot> </li>'
 })
 
 new Vue({
     el: '#root'
-
 })
